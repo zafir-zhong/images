8a4e732c42dabc18b06e572d7694473f90220eef
zhongzixuan <1309387239@qq.com>
2021-10-08 21:08:12 +0800

    提交脚本

---
+++ download.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+image=${1}
+tmp_image=${image#*\/}
+gen_image=${tmp_image//[\/:]/-}
+
+echo "will download this image from: registry.cn-hangzhou.aliyuncs.com/zafir-zhong/images:${gen_image}"
+docker pull registry.cn-hangzhou.aliyuncs.com/zafir-zhong/images:${gen_image}
+docker tag registry.cn-hangzhou.aliyuncs.com/zafir-zhong/images:${gen_image} ${image}
+docker rmi registry.cn-hangzhou.aliyuncs.com/zafir-zhong/images:${gen_image}
+echo -e ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> download successfully!\n"
