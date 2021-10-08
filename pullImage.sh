b4d388e1ad8072ab78c6025f6dbc526d483b3c04
zhongzixuan <1309387239@qq.com>
2021-10-08 21:41:50 +0800

    提交补充和优化的脚本

---
+++ pullImage.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+image=${1}
+tmp_image=${image#*\/}
+gen_image=${tmp_image//[\/:]/-}
+
+
+echo "will pull this image from: registry.cn-hangzhou.aliyuncs.com/zafir-zhong/images:${gen_image}"
+docker pull registry.cn-hangzhou.aliyuncs.com/zafir-zhong/images:${gen_image}
+image=`docker images | grep ${gen_image}`
+if [ -n "${image}" ];
+then
+  echo -e ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> download successfully!\n"
+  docker tag registry.cn-hangzhou.aliyuncs.com/zafir-zhong/images:${gen_image} ${image}
+  docker rmi registry.cn-hangzhou.aliyuncs.com/zafir-zhong/images:${gen_image}
+  echo -e ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> tags successfully!\n"
+else
+  echo -e ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> pull fail try commit and download!\n"
+  ./commit.sh ${image}
+  ./download.sh ${image}
+fi
